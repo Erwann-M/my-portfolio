66202f170b9dbe807141ed9d86a50755545d0402
Erwann-M <rwnnmrtn@gmail.com>
2022-03-17 17:38:19 +0100

    contact finish

--- src/Components/Mails/MailJamie/index.jsx
+++ src/Components/Mails/MailJamie/index.jsx
@@ -13,10 +13,10 @@ const MailJamie = () => (
     <h3 className="mail__object">Réponse à ta question</h3>
     <p className="mail__content">
       Salut Erwann, voici ce que tu m'avais demandé: <br/><br/>
-      - adresse e-mail: <a href="mailto:erwann.martin.dev@gmail.com">erwann.martin.dev@gmail.com</a><br/><br/>
+      - Adresse e-mail: <a href="mailto:erwann.martin.dev@gmail.com">erwann.martin.dev@gmail.com</a><br/><br/>
       - LinkedIn: <a href="https://www.linkedin.com/in/erwann-martin/">https://www.linkedin.com/in/erwann-martin/</a><br/><br/>
       - Twitter: <a href="https://twitter.com/ErwannMartin3">https://twitter.com/ErwannMartin3</a><br/><br/>
-      - secteurs de recherche: Côtes d'armor, toute la france (remote)
+      - Secteurs de recherche: Côtes d'armor, toute la france (remote)
     </p>
 
   </div>
