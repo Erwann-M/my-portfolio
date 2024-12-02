a77773d0d480f1130d0d42bf2ba1935e158634af
Erwann-M <rwnnmrtn@gmail.com>
2024-12-02 20:58:54 +0100

    maj jobs

--- src/Components/About/AboutCardsDetails/Work/index.jsx
+++ src/Components/About/AboutCardsDetails/Work/index.jsx
@@ -1,5 +1,7 @@
 import siiLogo from '../../../../assets/icons/SII_Logo.png';
 import bouyguesLogo from '../../../../assets/icons/bouygues.png';
+import naturlyLogo from '../../../../assets/icons/naturly-logo.png';
+import lnsLogo from '../../../../assets/img/longNeck-icon.png';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleWork } from '../../../../action';
@@ -18,6 +20,34 @@ const Work = () => {
   return (
     <section className={"work " + (workToggled && "active")} onClick={handleClickWork}>
       <div className='work__container'>
+        <img src={naturlyLogo} alt="" className="work__container__image" />
+        <h4 className="work__container__title">[sept 2024/nov 2024] Mission freelance : Naturly</h4>
+      </div>
+      <div className='work__container__detail'>
+        <p className="work__container__detail__text">
+          Mon premier projet en freelance a été réalisé pour un client souhaitant intégrer une page interactive avec un mini-jeu à son site web. L’objectif était d’attirer de nouveaux utilisateurs de manière ludique et engageante. La demande se distinguait par son originalité et nécessitait une approche créative et féerique, en harmonie avec l’identité du site, qui faisait la promotion de leur application mobile. Ce projet m’a permis de mettre toute ma créativité au service du client, et a été une expérience enrichissante tant sur le plan technique qu’humain.
+        </p>
+        <p className="work__container__detail__text">
+          <strong className='bold'>Ajout d'une page interactive :</strong> Conception et intégration d'une page supplémentaire en WordPress avec Elementor Pro, enrichie par des animations personnalisées en JavaScript pour répondre précisément aux attentes du client.
+        </p>
+        <p className="work__container__detail__text">
+          <strong className='bold'>Développement d'un thème personnalisé :</strong> Création d'un thème WordPress sur mesure dédié à la page du mini-jeu, conçu pour s'aligner avec l'identité visuelle et les objectifs du projet.
+        </p>
+        <p className="work__container__detail__text">
+          <strong className='bold'>Création d'un mini-jeu :</strong> Développement d'un mini-jeu interactif intégré au site, conçu pour attirer les utilisateurs vers l'application finale et collecter des adresses e-mail, tout en offrant une expérience ludique et engageante.
+        </p>
+        <p className="work__container__detail__text">
+          <strong className='bold'>Plugin WordPress sur mesure :</strong> Développement d'un plugin dédié au mini-jeu, permettant d’enregistrer les nouveaux utilisateurs en base de données et de leur envoyer un e-mail automatisé immédiatement après leur participation.
+        </p>
+        <p className="work__container__detail__text">
+          <strong className='bold'>Intégration d'un service de messagerie :</strong> Connexion et configuration du système d'envoi d'e-mails pour une communication efficace et un suivi optimisé des utilisateurs inscrits.
+        </p>
+        <p className="work__container__detail__text">
+          L’ensemble de ces développements a permis de proposer une solution interactive et ciblée, répondant parfaitement aux besoins marketing et techniques du client.
+        </p>        
+      </div>
+      
+      <div className='work__container'>
         <img src={siiLogo} alt="" className="work__container__image" />
         <h4 className="work__container__title">[2021/2022] - SII Group:</h4>
       </div>
@@ -30,19 +60,34 @@ const Work = () => {
           J'ai débuté mon poste en mai 2021 en tant que développeur sur le projet de Bouygues Télécom. Je ne connaissais pas le langage Python ni l'automatisation de test en arrivant sur cette mission. Très vite, j'ai su m'adapter aux différents langages, outils et process.
         </p>
         <p className="work__container__detail__text">
-          Pendant cette expérience, j'ai pris énormément confiance en moi et mes capacités. Avant cette mission, étant autodidacte et sortant d'une formation accélérée, j'avais bien conscience que mon profil n'était pas ragoutant. Pourtant, cette entreprise m'a fait confiance et n'a pas été déçue. <strong className='bold'>Je garde</strong> d'ailleurs <strong className='bold'>un très bon contact avec mon équipe, mon directeur technique, mon chef de projet et mon responsable d'agence</strong>. 
+          Pendant cette expérience, j'ai pris énormément confiance en moi et mes capacités. Avant cette mission, étant autodidacte et sortant d'une formation accélérée, j'avais bien conscience que mon profil n'était pas ragoutant. Pourtant, cette entreprise m'a fait confiance et n'a pas été déçue. Je garde d'ailleurs <strong className='bold'>un très bon contact avec mon équipe, mon directeur technique, mon chef de projet et mon responsable d'agence</strong>. 
         </p>
         <p className="work__container__detail__text">
           <strong className='bold'>Ils m'ont proposé de donner leurs contacts à mes futurs recruteurs</strong>. Afin de me recommander, d'attester de mes capacités et de me donner une chance de trouver un poste qui me correspond. Je suis très reconnaissant envers eux et je les remercie pour tout ce qu'ils m'ont apporté.
         </p>
         <p className="work__container__detail__text">
-          Pendant cette expérience, j'ai abordé beaucoup de sujets. J'ai appris l'automatisation de test Réseaux et télécom, et j'ai créé des interfaces web afin de consulter les résultats de ces tests. Je ne peux pas montrer le fruit de mon travail ici, car ce sont des outils internes à Bouygues et je suis donc dans l'interdiction de communiquer en détail dessus.
+          Pendant cette expérience, j'ai abordé beaucoup de sujets: 
         </p>
         <p className="work__container__detail__text">
-          J'ai travaillé sur des API, sur du cœur de réseau (maquette), des bases de données type MongoDB ou MySQL, des interfaces web en html/css/js parfois sur le framework Django ou Flask. J'ai fait autant de front-end que de back-end, mais aussi de l'automatisation de test grâce au langage python, le logiciel itest et l'ordonnanceur Jenkins. La plupart des projets étaient de la maintenance ou de l'ajout de fonctionnalités. Après avoir gagner la confiance de mon équipe et des mes manager, quelques projets m'ont été confiés du début à la fin et j'ai pu les mener à bien.
+          <strong className='bold'>Automatisation de tests réseau et télécom :</strong> Développement de scripts et de processus pour automatiser les tests, réduisant les erreurs humaines et augmentant la productivité.
         </p>
         <p className="work__container__detail__text">
-          Pour finir, je tire beaucoup de positif de cette expérience. J'ai appris énormément de choses, j'ai pris confiance en moi et j'ai pu travailler avec des personnes très compétentes et très sympathiques. Je suis très reconnaissant envers cette entreprise. Mais il est temps pour moi de voguer vers de nouvelles aventures et dans un domaine qui m'a toujours plus : le développement web.
+          <strong className='bold'>Développement d’interfaces graphiques :</strong> Conception et réalisation d’outils visuels pour simplifier et accélérer les tâches répétitives.
+        </p>
+        <p className="work__container__detail__text">
+          <strong className='bold'>Campagnes de tests pour les box internet :</strong> Mise en place de scénarios de tests exhaustifs, incluant des vérifications de non-régression, pour garantir la qualité et la fiabilité des box.
+        </p>
+        <p className="work__container__detail__text">
+          <strong className='bold'>Validation des nouvelles offres 4G/5G :</strong> Conception et réalisation de tests prenant en compte la non-régression, pour valider les nouvelles offres de forfaits mobiles tout en répondant aux exigences réseau et aux attentes des clients.
+        </p>
+        <p className="work__container__detail__text">
+          <strong className='bold'>Tests pour équipements ONT/OLT :</strong> Conception et mise en œuvre de campagnes de tests approfondis pour garantir la conformité et la performance des terminaux et équipements optiques au sein de l’infrastructure réseau.
+        </p>
+        <p className="work__container__detail__text">
+          <strong className='bold'>Outil de provisionning/deprovisionning en masse :</strong> Création d’un outil complet, de la conception à la mise en production, pour gérer automatiquement de grandes quantités de cartes SIM.
+        </p>
+        <p className="work__container__detail__text">
+          Ces expériences m’ont permis de développer des compétences pointues en automatisation, en validation de systèmes complexes, et en gestion d’outils critiques pour la performance des services.
         </p>
         <p className="work__container__detail__text">
           <strong className='bold'>Technologies abordées:</strong>
@@ -50,6 +95,34 @@ const Work = () => {
         <p className="work__container__detail__text">
           html, css, javascript, groovy script, python, tcl, gitlab, gitlab CI, django, flask, itest, jenkins, mongodb, mysql, jira, confluence, ...
         </p>
+      </div>
+
+      <div className='work__container'>
+        <img src={lnsLogo} alt="" className="work__container__image" />
+        <h4 className="work__container__title">[juil 2022/aout 2022] Développeur web 3: Long Neck Society</h4>
+      </div>
+      <div className='work__container__detail'>
+        <p className="work__container__detail__text">
+          J’ai participé à la création d’un projet de NFT, en intervenant sur plusieurs aspects techniques et créatifs :
+        </p>
+        <p className="work__container__detail__text">
+          <strong className='bold'>Création des visuels :</strong> Réalisation de dessins modulaires (yeux, bouches, corps, arrière-plans, etc.) qui ont été combinés aléatoirement via un programme pour générer une collection unique de 5 555 images, chaque NFT possédant des attributs avec des niveaux de rareté variés.
+        </p> 
+        <p className="work__container__detail__text">
+          <strong className='bold'>Génération des métadonnées :</strong> Association de chaque image à un fichier JSON contenant ses attributs (ex. : rareté, type) et un numéro unique.
+        </p> 
+        <p className="work__container__detail__text">
+          <strong className='bold'>Inscription sur la blockchain :</strong> Développement d’un script automatisé pour stocker les images sur un serveur, associer chaque fichier JSON à son NFT, et inscrire l’ensemble de la collection sur la blockchain.
+        </p> 
+        <p className="work__container__detail__text">
+          <strong className='bold'>Développement du site :</strong> Création d’un site web de présentation de la collection et d’une page de mint.
+        </p>   
+        <p className="work__container__detail__text">
+          <strong className='bold'>Page de mint :</strong> Plateforme permettant aux utilisateurs d’acquérir un NFT directement depuis la blockchain. Le processus de mint consiste à inscrire un NFT à l’utilisateur, générant ainsi une preuve de propriété directement sur la blockchain.
+        </p>
+        <p className="work__container__detail__text">
+          Ce projet m’a permis de combiner compétences techniques (programmation, blockchain) et créatives (design graphique) tout en contribuant à une expérience utilisateur complète, de la création des visuels à leur mise en ligne et acquisition.
+        </p>      
       </div>
       
     </section>
